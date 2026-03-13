bc1a3db598f35d68af99dc4684c03866db480468
Celso Serrano Araujo <celso.saraujo@hotmail.com>
2026-03-13 19:49:39 -0300

    PHP Básico

---
+++ basico/01variaveis.php
@@ -0,0 +1,24 @@
+<?php
+
+    $nome = "Carlos"; //String
+    $idade = 20; //int
+    $altura = 1.75; //double
+    $ativo = true; //boolean
+
+?>
+
+<!DOCTYPE html>
+ <html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Declaração de Variáveis</title>
+    </head>
+    <body>
+        <?php
+            echo "<br>Nome: $nome";
+            echo "<br>Idade: $idade";
+        ?>       
+    </body>
+
+ </html>
